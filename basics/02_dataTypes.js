e0b73e85f5a2c6722bdbb7c8411cca36353533ac
sumeetmazumdar21 <sumeet.mazumdar.21@gmail.com>
2024-08-10 08:00:05 +0000

    JS Datatypes

---
+++ basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all JS as newer version.
+
+// alert( 3 + 3) --> we're using nodeJS, not browser. Alert doesn't work this way in node.
+
+// number => 2^5 -1
+// bigInt
+// string => "abc"
+// boolean => true/false
+// null => standalone value
+// undefined=> declared but not defined. eg: let age; 
+// symbol => for defining uniqueness
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
+
+
