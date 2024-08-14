b4fd42907036c26f08d39580075c03c618b7ea6f
sumeetmazumdar21 <sumeet.mazumdar.21@gmail.com>
2024-08-14 10:57:07 +0530

    Update 06_nums_and_math.js

--- basics/06_nums_and_math.js
+++ basics/06_nums_and_math.js
@@ -41,7 +41,7 @@
     const min = 10;
     const max = 20;
 
-    console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+    console.log(Math.floor(Math.random() * (max - min + 1)) + min); // values between min and max => min <= value <= max
     
     
     
