4c1c108e1b5281afdabae4751ca77aadb08eb076
sumeetmazumdar21 <sumeet.mazumdar.21@gmail.com>
2024-08-16 11:39:59 +0000

    Spread Operators and handling objects in function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -39,4 +39,50 @@
     console.log(loginMessage("Sam Pitroda"));
         //  Welcome, Sam Pitroda
  
-    
+//     //++++++++++++++++++++++++++++++++++++ 
+
+/*Rest parameters "..." in function(...parameters)
+    The rest parameter syntax allows a function to accept an indefinite number of arguments as an array, providing a way to represent variadic functions in JavaScript.
+*/
+
+    function cartTotal(...num){
+        return num;
+    }
+    console.log(cartTotal(100, 500, 400, 2000, 2300)); // [ 100, 500, 400, 2000, 2300 ]
+
+    function cartTotal(val1, val2, ...num){
+        return num;
+    }
+    console.log(cartTotal(100, 500, 400, 2000, 2300)); // [ 400, 2000, 2300 ]
+    // val1 = 100, val2 = 500, ...num = [400, 2000, 2300]
+
+// Handling objects using function
+
+    function handleProduct(objectName){
+        console.log(`Product name is ${objectName.productName} and Product Price is INR ${objectName.productPrice}`); 
+    }
+
+    let product = {
+        productName: "iPad",
+        productPrice: 84999
+    }
+
+    handleProduct(product); // Product name is iPad and Product Price is INR 84999
+
+        
+    handleProduct({
+        productName: "Oppo 15 Pro",
+        productPrice: 21000
+    });  // Product name is Oppo 15 Pro and Product Price is INR 21000
+
+
+// Handling Array in function
+
+    let arr = [100, 200, 300, 400, 500];
+
+    function returnSecondValue(getArr){
+        return getArr[1];
+    } // console.log(returnSecondValue(arr)); // Output => 200
+
+
+    console.log(returnSecondValue([23, 45, 11, 89, 67])); // Output => 45
